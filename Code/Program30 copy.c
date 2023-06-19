4a01965f6242dad52c0bd64e9594c92b7f4cfc7d
Shubham Hemant Vaidya <105340317+ShubhamVaidya26@users.noreply.github.com>
2023-06-19 18:37:01 +0530

    Add files via upload

---
+++ Code/Program30 copy.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool CheckPrime(int iNo)
+{
+
+    int iCnt = 0;
+    bool bFlag = true;
+
+    for (iCnt = 2; iCnt <= (iNo / 2); iCnt++)  //time complexity (N/2) if number is prime number
+    {
+
+        if ((iNo % iCnt) == 0)
+        {
+            bFlag = false;
+            break;
+        }
+    }
+    return bFlag;
+}
+
+int main()
+{
+
+    int iValue = 0;
+    int bRet = false;
+
+    printf("Enter number :\n");
+    scanf("%d", &iValue);
+
+    bRet = CheckPrime(iValue);
+
+    if (bRet == true)
+    {
+        printf("%d is prime number\n", iValue);
+    }
+    else
+    {
+        printf("%d is not a prime number\n", iValue);
+    }
+
+    return 0;
+}
